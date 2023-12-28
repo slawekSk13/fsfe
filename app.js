da0e26c86826e6c10520d992dadf2357f168d1a2
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2023-12-28 21:58:25 +0100

    websockets

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const http = require('http');
+const express = require('express');
+const server = require('http').createServer();
+const WebSocketServer = require('ws').Server;
+const PORT = 3001;
 
-http.createServer(function(req, res){
-res.write("It is indeed, right?");
-res.end();
-}).listen(3000);
+const app = express();
 
-console.log("Server started on port 3000");
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {root: __dirname});
+});
+
+server.on('request', app);
+server.listen(PORT, function () {
+  console.log('Server started on port ' + PORT);
+});
+
+const wss = new WebSocketServer({server});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function (client) {
+    client.send(data);
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  const clientsCount = wss.clients.size;
+  console.log(`Clients connected, count: ${clientsCount}`);
+  wss.broadcast(`Current clients count: ${clientsCount}`);
+
+  if (ws.readyState === ws.OPEN) ws.send('Welcome to my server');
+
+  ws.on('close', function close() {
+    console.log('Disconnected');
+  });
+});
